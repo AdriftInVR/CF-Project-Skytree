2bca46556bf027c0d8f364ccaacea8db777a69ba
Leonardo Ramos <a01707812@itesm.mx>
2024-09-27 01:05:35 -0600

    Animaciones de combate de timber implementadas

--- Rooted Psyche/Assets/Scripts/Jugadores/Brier.cs
+++ Rooted Psyche/Assets/Scripts/Jugadores/Brier.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Brier : Fighter{
-
     [Header("UI")]
     public PlayerSkillPanel skillPanel;
+    
+    private Animator animator; // Referencia al Animator
 
-    void Awake(){
-
-        this.stats = new Stats(100, 100, 10, 10, 51, 1, 10, 1.0f, 1.0f);
+    void Awake()
+    {
+        this.stats = new Stats(100, 100, 10, 10, 50, 1, 10, 1.0f, 1.0f);
+        this.animator = GetComponent<Animator>(); // Obtén el Animator
     }
+
     public override void InitTurn(){
 
         this.skillPanel.Show();
         for (int i = 0; i < this.skills.Length; i++){
-
             this.skillPanel.ConfigureButtons(i, this.skills[i].skillName);
         }
     }
@@ -26,9 +28,12 @@ public class Brier : Fighter{
 
         skill.SetEmmiterAndReciver(this, this.combatManager.GetOppositeFighter());
 
+        // Llama a la animación de ataque
+        animator.SetTrigger("Attack");
+
+        // Después de ejecutar la animación, realiza la habilidad
         this.combatManager.OnFighterSkill(skill);
 
         Debug.Log("Ejecutando habilidad: " + skill.skillName);
-
     }
 }
