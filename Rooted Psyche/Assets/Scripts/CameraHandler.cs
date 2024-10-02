443a094416115d721cbb94d3f939f5b8ccc5c591
AdriftInVR <sebastian.p.jimenez@gmail.com>
2024-10-02 11:30:21 -0600

    feat(Exploration): Basic movement for overworld

--- Rooted Psyche/Assets/Scripts/CameraHandler.cs
+++ Rooted Psyche/Assets/Scripts/CameraHandler.cs
@@ -10,7 +10,7 @@ public class CameraHandler : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        Physics.gravity = new Vector3(0,-20f,0);
+        
     }
 
     // Update is called once per frame
--- Rooted Psyche/Assets/Scripts/ExplorationController.cs
+++ Rooted Psyche/Assets/Scripts/ExplorationController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class TestController : MonoBehaviour
+public class ExplorationController : MonoBehaviour
 {
     Rigidbody rb;
     public bool canJump = true;
 
+    
+    [SerializeField]
+    private Transform cam;
+
     [SerializeField]
     private float spd, jumpSpd;
     private float sqrspd;
-    public float decelerationMultiplier = 0.5f;
 
     PlayerInput myInput;
 
@@ -26,14 +29,26 @@ public class TestController : MonoBehaviour
     {
         Vector2 direction2D = myInput.actions["Direction"].ReadValue<Vector2>();
         direction2D.Normalize();
-        Vector3 direction3D = new Vector3(direction2D.x, 0, direction2D.y);
+
+        Vector3 camF = cam.forward;
+        Vector3 camR = cam.right;
+
+        camF.y = 0;
+        camR.y = 0;
+
+        Vector3 relF = camF * direction2D.y;
+        Vector3 relR = camR * direction2D.x;
+
+        Vector3 moveDir = relF + relR;
+
+        Vector3 direction3D = new Vector3(moveDir.x, 0, moveDir.z);
 
         var v = rb.velocity;
 
         // Check if the velocity vector is greater than the maximum allowed
         if(v.sqrMagnitude > sqrspd){
             // Trunk the excess speed back to the limit with the normalized vector
-            rb.velocity = new Vector3(direction2D.x * spd, rb.velocity.y, direction2D.y * spd);
+            rb.velocity = new Vector3(moveDir.x * spd, rb.velocity.y, moveDir.z * spd);
         } 
 
         if(myInput.actions["Direction"].ReadValue<Vector2>().sqrMagnitude > 0f)
@@ -51,9 +66,8 @@ public class TestController : MonoBehaviour
 
     private void OnJump(){
         if(canJump){
-            rb.AddForce(new Vector3(0,jumpSpd,0));
+            rb.velocity = new Vector3(0,jumpSpd,0);
             canJump = false;
-            Debug.Log(rb.velocity);
         }
     }
 
@@ -64,6 +78,21 @@ public class TestController : MonoBehaviour
     }
 
     private void OnCollisionEnter(Collision col) {
-        canJump = true;
+        
+        // Print how many points are colliding with this transform
+        Debug.Log("Points colliding: " + col.contacts.Length);
+
+        // Print the normal of the first point in the collision.
+        Debug.Log("Normal of the first point: " + col.contacts[0].normal);
+
+        // Draw a different colored ray for every normal in the collision
+        foreach (var item in col.contacts)
+        {
+            Debug.DrawRay(item.point, item.normal * 100, Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f), 10f);
+        }
+        if(col.gameObject.tag == "Floor")
+        {
+            canJump = true;
+        }
     }
 }
