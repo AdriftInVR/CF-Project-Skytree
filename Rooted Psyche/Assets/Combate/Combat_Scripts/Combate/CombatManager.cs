6dcd72d2425e6cb5f229dd726e34da019aca6670
Leonardo Ramos <a01707812@itesm.mx>
2024-11-22 00:04:53 -0600

    death scene

--- Rooted Psyche/Assets/Combate/Combat_Scripts/Combate/CombatManager.cs
+++ Rooted Psyche/Assets/Combate/Combat_Scripts/Combate/CombatManager.cs
@@ -179,6 +179,7 @@ public class CombatManager : MonoBehaviour{
         {
             end = true;
             Debug.Log("El equipo del jugador ha sido derrotado.");
+            SceneManager.LoadScene("Derrota");
         }
         if (enemiesAlive == 0)
         {
